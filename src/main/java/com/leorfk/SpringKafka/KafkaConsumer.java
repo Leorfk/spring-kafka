1a089173a4064303e57b66d8fe2088417c63d89f
leorfk <leonardorodrigues_f@hotmail.com>
2021-07-21 23:44:47 -0300

    adicionando customizaçõs no header da mensagem

--- src/main/java/com/leorfk/SpringKafka/KafkaConsumer.java
+++ src/main/java/com/leorfk/SpringKafka/KafkaConsumer.java
@@ -2,20 +2,30 @@ package com.leorfk.SpringKafka;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "${topic.name}", groupId = "group_id")
+    @KafkaListener(topics = "${topic.name}")
     public void consume(ConsumerRecord<String, String> payload) {
         log.info("Tópico: {}", payload.topic());
         log.info("key: {}", payload.key());
         log.info("Headers: {}", payload.headers());
         log.info("Partion: {}", payload.partition());
         log.info("Order: {}", payload.value());
+        getKafkaMessageHeaderContent(payload);
+    }
+
+    private void getKafkaMessageHeaderContent(ConsumerRecord payload){
+        for (var header: payload.headers()) {
+            log.info("{}: {}", header.key(), new String(header.value(), StandardCharsets.UTF_8));
+        }
     }
 }
--- src/main/java/com/leorfk/SpringKafka/KafkaProducer.java
+++ src/main/java/com/leorfk/SpringKafka/KafkaProducer.java
@@ -1,10 +1,16 @@
 package com.leorfk.SpringKafka;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.UUID;
 
 @Slf4j
 @Service
@@ -17,8 +23,12 @@ public class KafkaProducer {
     private KafkaTemplate<String, String> kafkaTemplate;
 
     public void send(String message){
-        log.info("Payload enviado: {}", message);
-        kafkaTemplate.send(topicName, message);
+        var evento = new ProducerRecord<String, String>(topicName, message);
+        var data = new Date().toString();
+        evento.headers().add(KafkaHeaders.TIMESTAMP, data.getBytes());
+        evento.headers().add(KafkaHeaders.CORRELATION_ID, UUID.randomUUID().toString().getBytes());
+        evento.headers().add(new RecordHeader("transactionid", UUID.randomUUID().toString().getBytes()));
+        kafkaTemplate.send(evento);
     }
 
 }
