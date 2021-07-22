9d0a746a78ead3e2bd6126fbbbbfc2ca0707738a
leorfk <leonardorodrigues_f@hotmail.com>
2021-07-21 22:17:41 -0300

    criação do consumer

---
+++ src/main/java/com/leorfk/SpringKafka/KafkaConsumer.java
@@ -0,0 +1,21 @@
+package com.leorfk.SpringKafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "${topic.name}", groupId = "group_id")
+    public void consume(ConsumerRecord<String, String> payload) {
+        log.info("Tópico: {}", payload.topic());
+        log.info("key: {}", payload.key());
+        log.info("Headers: {}", payload.headers());
+        log.info("Partion: {}", payload.partition());
+        log.info("Order: {}", payload.value());
+    }
+}
--- src/main/java/com/leorfk/SpringKafka/KafkaProducer.java
+++ src/main/java/com/leorfk/SpringKafka/KafkaProducer.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaProducer {
 
-    @Value("${topic.name.producer}")
+    @Value("${topic.name}")
     private String topicName;
 
     @Autowired
